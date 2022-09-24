a1698510229e971582c28da5854453082638e2a1
Adriel Coria <99737740+Adriel3002@users.noreply.github.com>
2022-09-23 23:09:59 -0300

    terminando formularios

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const port = 3030;
 app.use (express.static('public'));
 
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')));
+
+
 /* app.get('/', (req,res) => res.send('Mercado Liebre'));
  */
 
